JuliusHerrmann/openairinterface5g

moving decision on DCI format type from L1 to MAC

openair1/PHY/NR_UE_TRANSPORT/dci_nr.c
@@ -43,15 +43,6 @@
 #include "assertions.h"
 #include "T.h"
 
-static const char nr_dci_format_string[8][30] = {"NR_DL_DCI_FORMAT_1_0",
-                                                 "NR_DL_DCI_FORMAT_1_1",
-                                                 "NR_DL_DCI_FORMAT_2_0",
-                                                 "NR_DL_DCI_FORMAT_2_1",
-                                                 "NR_DL_DCI_FORMAT_2_2",
-                                                 "NR_DL_DCI_FORMAT_2_3",
-                                                 "NR_UL_DCI_FORMAT_0_0",
-                                                 "NR_UL_DCI_FORMAT_0_1"};
-
 //#define DEBUG_DCI_DECODING 1
 
 //#define NR_PDCCH_DCI_DEBUG            // activates NR_PDCCH_DCI_DEBUG logs
@@ -780,24 +771,22 @@ static uint16_t nr_dci_false_detection(uint64_t *dci,
                                        int rnti,
                                        int8_t messageType,
                                        uint16_t messageLength,
-                                       uint8_t aggregation_level
-                                       ) {
-
+                                       uint8_t aggregation_level)
+{
   uint32_t encoder_output[NR_MAX_DCI_SIZE_DWORD];
-  polar_encoder_fast(dci, (void*)encoder_output, rnti, 1,
-                    messageType, messageLength, aggregation_level);
+  polar_encoder_fast(dci, (void *)encoder_output, rnti, 1, messageType, messageLength, aggregation_level);
   uint8_t *enout_p = (uint8_t*)encoder_output;
   uint16_t x = 0;
 
   for (int i=0; i<encoded_length/8; i++) {
-    x += ( enout_p[i] & 1 ) ^ ( ( soft_in[i*8] >> 15 ) & 1);
-    x += ( ( enout_p[i] >> 1 ) & 1 ) ^ ( ( soft_in[i*8+1] >> 15 ) & 1 );
-    x += ( ( enout_p[i] >> 2 ) & 1 ) ^ ( ( soft_in[i*8+2] >> 15 ) & 1 );
-    x += ( ( enout_p[i] >> 3 ) & 1 ) ^ ( ( soft_in[i*8+3] >> 15 ) & 1 );
-    x += ( ( enout_p[i] >> 4 ) & 1 ) ^ ( ( soft_in[i*8+4] >> 15 ) & 1 );
-    x += ( ( enout_p[i] >> 5 ) & 1 ) ^ ( ( soft_in[i*8+5] >> 15 ) & 1 );
-    x += ( ( enout_p[i] >> 6 ) & 1 ) ^ ( ( soft_in[i*8+6] >> 15 ) & 1 );
-    x += ( ( enout_p[i] >> 7 ) & 1 ) ^ ( ( soft_in[i*8+7] >> 15 ) & 1 );
+    x += (enout_p[i] & 1) ^ ((soft_in[i * 8] >> 15) & 1);
+    x += ((enout_p[i] >> 1) & 1) ^ ((soft_in[i * 8 + 1] >> 15) & 1);
+    x += ((enout_p[i] >> 2) & 1) ^ ((soft_in[i * 8 + 2] >> 15) & 1);
+    x += ((enout_p[i] >> 3) & 1) ^ ((soft_in[i * 8 + 3] >> 15) & 1);
+    x += ((enout_p[i] >> 4) & 1) ^ ((soft_in[i * 8 + 4] >> 15) & 1);
+    x += ((enout_p[i] >> 5) & 1) ^ ((soft_in[i * 8 + 5] >> 15) & 1);
+    x += ((enout_p[i] >> 6) & 1) ^ ((soft_in[i * 8 + 6] >> 15) & 1);
+    x += ((enout_p[i] >> 7) & 1) ^ ((soft_in[i * 8 + 7] >> 15) & 1);
   }
   return x;
 }
@@ -809,11 +798,11 @@ uint8_t nr_dci_decoding_procedure(PHY_VARS_NR_UE *ue,
                                   fapi_nr_dl_config_dci_dl_pdu_rel15_t *rel15)
 {
   //int gNB_id = 0;
-  int16_t tmp_e[16*108];
+  int16_t tmp_e[16 * 108];
   rnti_t n_rnti;
   int e_rx_cand_idx = 0;
 
-  for (int j=0;j<rel15->number_of_candidates;j++) {
+  for (int j = 0; j < rel15->number_of_candidates; j++) {
     int CCEind = rel15->CCE[j];
     int L = rel15->L[j];
 
@@ -822,48 +811,75 @@ uint8_t nr_dci_decoding_procedure(PHY_VARS_NR_UE *ue,
     for (int k = 0; k < rel15->num_dci_options; k++) {
       // skip this candidate if we've already found one with the
       // same rnti and format at a different aggregation level
-      int dci_found=0;
-      for (int ind=0;ind < dci_ind->number_of_dcis ; ind++) {
-        if (rel15->rnti== dci_ind->dci_list[ind].rnti &&
-            rel15->dci_format_options[k]==dci_ind->dci_list[ind].dci_format) {
-           dci_found=1;
-           break;
+      bool dci_found = false;
+      for (int ind = 0; ind < dci_ind->number_of_dcis; ind++) {
+        if (rel15->rnti == dci_ind->dci_list[ind].rnti && rel15->dci_format_options[k] == dci_ind->dci_list[ind].dci_format) {
+          dci_found = true;
+          break;
         }
       }
-      if (dci_found == 1)
+      if (dci_found)
         continue;
       int dci_length = rel15->dci_length_options[k];
-      uint64_t dci_estimation[2]= {0};
+      uint64_t dci_estimation[2] = {0};
 
-      LOG_D(PHY, "(%i.%i) Trying DCI candidate %d of %d number of candidates, CCE %d (%d), L %d, length %d, format %s\n",
-            proc->frame_rx, proc->nr_slot_rx, j, rel15->number_of_candidates, CCEind, e_rx_cand_idx, L, dci_length, nr_dci_format_string[rel15->dci_format_options[k]]);
+      LOG_D(PHY,
+            "(%i.%i) Trying DCI candidate %d of %d number of candidates, CCE %d (%d), L %d, length %d, format %d\n",
+            proc->frame_rx,
+            proc->nr_slot_rx,
+            j,
+            rel15->number_of_candidates,
+            CCEind,
+            e_rx_cand_idx,
+            L,
+            dci_length,
+            rel15->dci_format_options[k]);
 
-
-      nr_pdcch_unscrambling(&pdcch_e_rx[e_rx_cand_idx], rel15->coreset.scrambling_rnti, L*108, rel15->coreset.pdcch_dmrs_scrambling_id, tmp_e);
+      nr_pdcch_unscrambling(&pdcch_e_rx[e_rx_cand_idx],
+                            rel15->coreset.scrambling_rnti,
+                            L * 108,
+                            rel15->coreset.pdcch_dmrs_scrambling_id,
+                            tmp_e);
 
 #ifdef DEBUG_DCI_DECODING
       uint32_t *z = (uint32_t *) &e_rx[e_rx_cand_idx];
-      for (int index_z = 0; index_z < L*6; index_z++){
-        for (int i=0; i<9; i++) {
-          LOG_I(PHY,"z[%d]=(%d,%d) \n", (9*index_z + i), *(int16_t *) &z[9*index_z + i],*(1 + (int16_t *) &z[9*index_z + i]));
+      for (int index_z = 0; index_z < L * 6; index_z++) {
+        for (int i = 0; i < 9; i++) {
+          LOG_I(PHY, "z[%d]=(%d,%d) \n", (9 * index_z + i), *(int16_t *)&z[9 * index_z + i], *(1 + (int16_t *)&z[9 * index_z + i]));
         }
       }
 #endif
-      uint16_t crc = polar_decoder_int16(tmp_e,
-                                         dci_estimation,
-                                         1,
-                                         NR_POLAR_DCI_MESSAGE_TYPE, dci_length, L);
+      uint16_t crc = polar_decoder_int16(tmp_e, dci_estimation, 1, NR_POLAR_DCI_MESSAGE_TYPE, dci_length, L);
 
       n_rnti = rel15->rnti;
-      LOG_D(PHY, "(%i.%i) dci indication (rnti %x,dci format %s,n_CCE %d,payloadSize %d,payload %llx )\n",
-            proc->frame_rx, proc->nr_slot_rx,n_rnti,nr_dci_format_string[rel15->dci_format_options[k]],CCEind,dci_length, *(unsigned long long*)dci_estimation);
+      LOG_D(PHY,
+            "(%i.%i) dci indication (rnti %x,dci format %d,n_CCE %d,payloadSize %d,payload %llx )\n",
+            proc->frame_rx,
+            proc->nr_slot_rx,
+            n_rnti,
+            rel15->dci_format_options[k],
+            CCEind,
+            dci_length,
+            *(unsigned long long *)dci_estimation);
       if (crc == n_rnti) {
-        LOG_D(PHY, "(%i.%i) Received dci indication (rnti %x,dci format %s,n_CCE %d,payloadSize %d,payload %llx)\n",
-              proc->frame_rx, proc->nr_slot_rx,n_rnti,nr_dci_format_string[rel15->dci_format_options[k]],CCEind,dci_length,*(unsigned long long*)dci_estimation);
+        LOG_D(PHY,
+              "(%i.%i) Received dci indication (rnti %x,dci format %d,n_CCE %d,payloadSize %d,payload %llx)\n",
+              proc->frame_rx,
+              proc->nr_slot_rx,
+              n_rnti,
+              rel15->dci_format_options[k],
+              CCEind,
+              dci_length,
+              *(unsigned long long *)dci_estimation);
         uint16_t mb = nr_dci_false_detection(dci_estimation,tmp_e,L*108,n_rnti, NR_POLAR_DCI_MESSAGE_TYPE, dci_length, L);
         ue->dci_thres = (ue->dci_thres + mb) / 2;
         if (mb > (ue->dci_thres+30)) {
-          LOG_W(PHY,"DCI false positive. Dropping DCI index %d. Mismatched bits: %d/%d. Current DCI threshold: %d\n",j,mb,L*108,ue->dci_thres);
+          LOG_W(PHY,
+                "DCI false positive. Dropping DCI index %d. Mismatched bits: %d/%d. Current DCI threshold: %d\n",
+                j,
+                mb,
+                L * 108,
+                ue->dci_thres);
           continue;
         } else {
           dci_ind->SFN = proc->frame_rx;
@@ -878,15 +894,22 @@ uint8_t nr_dci_decoding_procedure(PHY_VARS_NR_UE *ue,
           get_coreset_rballoc(rel15->coreset.frequency_domain_resource, &n_rb, &rb_offset);
           dci_ind->dci_list[dci_ind->number_of_dcis].cset_start = rel15->BWPStart + rb_offset;
           dci_ind->dci_list[dci_ind->number_of_dcis].payloadSize = dci_length;
-          memcpy((void*)dci_ind->dci_list[dci_ind->number_of_dcis].payloadBits,(void*)dci_estimation,8);
+          memcpy((void *)dci_ind->dci_list[dci_ind->number_of_dcis].payloadBits, (void *)dci_estimation, 8);
           dci_ind->number_of_dcis++;
           break;    // If DCI is found, no need to check for remaining DCI lengths
         }
       } else {
-        LOG_D(PHY,"(%i.%i) Decoded crc %x does not match rnti %x for DCI format %d\n", proc->frame_rx, proc->nr_slot_rx, crc, n_rnti, rel15->dci_format_options[k]);
+        LOG_D(PHY,
+              "(%i.%i) Decoded crc %x does not match rnti %x for DCI format %d\n",
+              proc->frame_rx,
+              proc->nr_slot_rx,
+              crc,
+              n_rnti,
+              rel15->dci_format_options[k]);
       }
     }
-    e_rx_cand_idx += 9*L*6*2; //e_rx index for next candidate (L CCEs, 6 REGs per CCE and 9 REs per REG and 2 uint16_t per RE)
+    e_rx_cand_idx += 9 * L * 6 * 2; // e_rx index for next candidate (L CCEs, 6 REGs per CCE and 9 REs per REG and 2 uint16_t per
+                                    // RE)
   }
   return(dci_ind->number_of_dcis);
 }

openair2/LAYER2/NR_MAC_UE/nr_ue_dci_configuration.c
@@ -101,7 +101,6 @@ NR_ControlResourceSet_t *ue_get_coreset(const NR_BWP_PDCCH_t *config, const int 
   return coreset;
 }
 
-
 void config_dci_pdu(NR_UE_MAC_INST_t *mac,
                     fapi_nr_dl_config_request_t *dl_config,
                     const int rnti_type,
@@ -154,36 +153,40 @@ void config_dci_pdu(NR_UE_MAC_INST_t *mac,
     rel15->coreset.pdcch_dmrs_scrambling_id = mac->physCellId;
   }
 
-  rel15->num_dci_options = (mac->ra.ra_state == WAIT_RAR || rnti_type == TYPE_SI_RNTI_) ? 1 : 2;
-
+  int temp_num_dci_options = (mac->ra.ra_state == WAIT_RAR || rnti_type == TYPE_SI_RNTI_) ? 1 : 2;
+  int dci_format[2] = {0};
   if (ss->searchSpaceType->present == NR_SearchSpace__searchSpaceType_PR_ue_Specific) {
     if (ss->searchSpaceType->choice.ue_Specific->dci_Formats ==
         NR_SearchSpace__searchSpaceType__ue_Specific__dci_Formats_formats0_0_And_1_0) {
-      rel15->dci_format_options[0] = NR_DL_DCI_FORMAT_1_0;
-      rel15->dci_format_options[1] = NR_UL_DCI_FORMAT_0_0;
+      dci_format[0] = NR_DL_DCI_FORMAT_1_0;
+      dci_format[1] = NR_UL_DCI_FORMAT_0_0;
     }
     else {
-      rel15->dci_format_options[0] = NR_DL_DCI_FORMAT_1_1;
-      rel15->dci_format_options[1] = NR_UL_DCI_FORMAT_0_1;
+      dci_format[0] = NR_DL_DCI_FORMAT_1_1;
+      dci_format[1] = NR_UL_DCI_FORMAT_0_1;
     }
   }
   else { // common
     AssertFatal(ss->searchSpaceType->choice.common->dci_Format0_0_AndFormat1_0,
                 "Only supporting format 10 and 00 for common SS\n");
-    rel15->dci_format_options[0] = NR_DL_DCI_FORMAT_1_0;
-    rel15->dci_format_options[1] = NR_UL_DCI_FORMAT_0_0;
+    dci_format[0] = NR_DL_DCI_FORMAT_1_0;
+    dci_format[1] = NR_UL_DCI_FORMAT_0_0;
   }
 
   NR_UE_ServingCell_Info_t *sc_info = &mac->sc_info;
-  // loop over RNTI type and configure resource allocation for DCI
-  for (int i = 0; i < rel15->num_dci_options; i++) {
+  // loop over DCI options and configure resource allocation
+  // need to configure mac->def_dci_pdu_rel15 for all possible format options
+  for (int i = 0; i < temp_num_dci_options; i++) {
     rel15->ss_type_options[i] = ss->searchSpaceType->present;
-    const int dci_format = rel15->dci_format_options[i];
+    if (dci_format[i] == NR_DL_DCI_FORMAT_1_0 || dci_format[i] == NR_UL_DCI_FORMAT_0_0)
+      rel15->dci_format_options[i] = NFAPI_NR_FORMAT_0_0_AND_1_0;
+    else
+      rel15->dci_format_options[i] = NFAPI_NR_FORMAT_0_1_AND_1_1;
     uint16_t alt_size = 0;
     if(current_DL_BWP) {
-      // computing alternative size for padding
+      // computing alternative size for padding or truncation
       dci_pdu_rel15_t temp_pdu;
-      if(dci_format == NR_DL_DCI_FORMAT_1_0)
+      if (dci_format[i] == NR_DL_DCI_FORMAT_1_0)
         alt_size = nr_dci_size(current_DL_BWP,
                                current_UL_BWP,
                                sc_info,
@@ -196,7 +199,7 @@ void config_dci_pdu(NR_UE_MAC_INST_t *mac,
                                ss->searchSpaceType->present,
                                mac->type0_PDCCH_CSS_config.num_rbs,
                                0);
-      if(dci_format == NR_UL_DCI_FORMAT_0_0)
+      if (dci_format[i] == NR_UL_DCI_FORMAT_0_0)
         alt_size = nr_dci_size(current_DL_BWP,
                                current_UL_BWP,
                                sc_info,
@@ -215,8 +218,8 @@ void config_dci_pdu(NR_UE_MAC_INST_t *mac,
                                                current_UL_BWP,
                                                sc_info,
                                                mac->pdsch_HARQ_ACK_Codebook,
-                                               &mac->def_dci_pdu_rel15[dl_config->slot][dci_format],
-                                               dci_format,
+                                               &mac->def_dci_pdu_rel15[dl_config->slot][dci_format[i]],
+                                               dci_format[i],
                                                rnti_type,
                                                coreset,
                                                dl_bwp_id,
@@ -224,6 +227,13 @@ void config_dci_pdu(NR_UE_MAC_INST_t *mac,
                                                mac->type0_PDCCH_CSS_config.num_rbs,
                                                alt_size);
   }
+
+  // DCI 0_0 and 1_0 are same size, L1 just needs to look for 1 option
+  // L2 decides format based on format indicator in payload
+  if (rel15->dci_format_options[0] == NFAPI_NR_FORMAT_0_0_AND_1_0)
+    rel15->num_dci_options = 1;
+  else
+    rel15->num_dci_options = 2;
 
   rel15->BWPStart = coreset_id == 0 ? mac->type0_PDCCH_CSS_config.cset_start_rb : current_DL_BWP->BWPStart;
   rel15->BWPSize = coreset_id == 0 ? mac->type0_PDCCH_CSS_config.num_rbs : current_DL_BWP->BWPSize;

Nyanm/oai-ran-tlab

openair1/PHY/NR_UE_TRANSPORT/dci_nr.c
@@ -43,15 +43,6 @@
 #include "assertions.h"
 #include "T.h"
 
-static const char nr_dci_format_string[8][30] = {"NR_DL_DCI_FORMAT_1_0",
-                                                 "NR_DL_DCI_FORMAT_1_1",
-                                                 "NR_DL_DCI_FORMAT_2_0",
-                                                 "NR_DL_DCI_FORMAT_2_1",
-                                                 "NR_DL_DCI_FORMAT_2_2",
-                                                 "NR_DL_DCI_FORMAT_2_3",
-                                                 "NR_UL_DCI_FORMAT_0_0",
-                                                 "NR_UL_DCI_FORMAT_0_1"};
-
 //#define DEBUG_DCI_DECODING 1
 
 //#define NR_PDCCH_DCI_DEBUG            // activates NR_PDCCH_DCI_DEBUG logs
@@ -699,24 +690,22 @@ static uint16_t nr_dci_false_detection(uint64_t *dci,
                                        int rnti,
                                        int8_t messageType,
                                        uint16_t messageLength,
-                                       uint8_t aggregation_level
-                                       ) {
-
+                                       uint8_t aggregation_level)
+{
   uint32_t encoder_output[NR_MAX_DCI_SIZE_DWORD];
-  polar_encoder_fast(dci, (void*)encoder_output, rnti, 1,
-                    messageType, messageLength, aggregation_level);
+  polar_encoder_fast(dci, (void *)encoder_output, rnti, 1, messageType, messageLength, aggregation_level);
   uint8_t *enout_p = (uint8_t*)encoder_output;
   uint16_t x = 0;
 
   for (int i=0; i<encoded_length/8; i++) {
-    x += ( enout_p[i] & 1 ) ^ ( ( soft_in[i*8] >> 15 ) & 1);
-    x += ( ( enout_p[i] >> 1 ) & 1 ) ^ ( ( soft_in[i*8+1] >> 15 ) & 1 );
-    x += ( ( enout_p[i] >> 2 ) & 1 ) ^ ( ( soft_in[i*8+2] >> 15 ) & 1 );
-    x += ( ( enout_p[i] >> 3 ) & 1 ) ^ ( ( soft_in[i*8+3] >> 15 ) & 1 );
-    x += ( ( enout_p[i] >> 4 ) & 1 ) ^ ( ( soft_in[i*8+4] >> 15 ) & 1 );
-    x += ( ( enout_p[i] >> 5 ) & 1 ) ^ ( ( soft_in[i*8+5] >> 15 ) & 1 );
-    x += ( ( enout_p[i] >> 6 ) & 1 ) ^ ( ( soft_in[i*8+6] >> 15 ) & 1 );
-    x += ( ( enout_p[i] >> 7 ) & 1 ) ^ ( ( soft_in[i*8+7] >> 15 ) & 1 );
+    x += (enout_p[i] & 1) ^ ((soft_in[i * 8] >> 15) & 1);
+    x += ((enout_p[i] >> 1) & 1) ^ ((soft_in[i * 8 + 1] >> 15) & 1);
+    x += ((enout_p[i] >> 2) & 1) ^ ((soft_in[i * 8 + 2] >> 15) & 1);
+    x += ((enout_p[i] >> 3) & 1) ^ ((soft_in[i * 8 + 3] >> 15) & 1);
+    x += ((enout_p[i] >> 4) & 1) ^ ((soft_in[i * 8 + 4] >> 15) & 1);
+    x += ((enout_p[i] >> 5) & 1) ^ ((soft_in[i * 8 + 5] >> 15) & 1);
+    x += ((enout_p[i] >> 6) & 1) ^ ((soft_in[i * 8 + 6] >> 15) & 1);
+    x += ((enout_p[i] >> 7) & 1) ^ ((soft_in[i * 8 + 7] >> 15) & 1);
   }
   return x;
 }
@@ -730,7 +719,7 @@ void nr_dci_decoding_procedure(PHY_VARS_NR_UE *ue,
   int e_rx_cand_idx = 0;
   *dci_ind = (fapi_nr_dci_indication_t){.SFN = proc->frame_rx, .slot = proc->nr_slot_rx};
 
-  for (int j=0;j<rel15->number_of_candidates;j++) {
+  for (int j = 0; j < rel15->number_of_candidates; j++) {
     int CCEind = rel15->CCE[j];
     int L = rel15->L[j];
 
@@ -751,7 +740,7 @@ void nr_dci_decoding_procedure(PHY_VARS_NR_UE *ue,
       uint64_t dci_estimation[2]= {0};
 
       LOG_D(NR_PHY_DCI,
-            "(%i.%i) Trying DCI candidate %d of %d number of candidates, CCE %d (%d), L %d, length %d, format %s\n",
+            "(%i.%i) Trying DCI candidate %d of %d number of candidates, CCE %d (%d), L %d, length %d, format %d\n",
             proc->frame_rx,
             proc->nr_slot_rx,
             j,
@@ -760,31 +749,31 @@ void nr_dci_decoding_procedure(PHY_VARS_NR_UE *ue,
             e_rx_cand_idx,
             L,
             dci_length,
-            nr_dci_format_string[rel15->dci_format_options[k]]);
+            rel15->dci_format_options[k]);
 
       int16_t tmp_e[16 * 108];
-      nr_pdcch_unscrambling(&pdcch_e_rx[e_rx_cand_idx], rel15->coreset.scrambling_rnti, L*108, rel15->coreset.pdcch_dmrs_scrambling_id, tmp_e);
-      // this polar version decodes 64 bits max, dci_estimation[1] will never be filled
-      uint16_t crc = polar_decoder_int16(tmp_e,
-                                         dci_estimation,
-                                         1,
-                                         NR_POLAR_DCI_MESSAGE_TYPE, dci_length, L);
+      nr_pdcch_unscrambling(&pdcch_e_rx[e_rx_cand_idx],
+                            rel15->coreset.scrambling_rnti,
+                            L * 108,
+                            rel15->coreset.pdcch_dmrs_scrambling_id,
+                            tmp_e);
+
+      uint16_t crc = polar_decoder_int16(tmp_e, dci_estimation, 1, NR_POLAR_DCI_MESSAGE_TYPE, dci_length, L);
 
       rnti_t n_rnti = rel15->rnti;
-      LOG_D(NR_PHY_DCI,
-            "(%i.%i) dci indication (rnti %x,dci format %s,n_CCE %d,payloadSize %d,payload %lx, is rnti: %d )\n",
-            proc->frame_rx,
-            proc->nr_slot_rx,
-            n_rnti,
-            nr_dci_format_string[rel15->dci_format_options[k]],
-            CCEind,
-            dci_length,
-            dci_estimation[0],
-            crc == n_rnti);
       if (crc == n_rnti) {
-        uint16_t mb = nr_dci_false_detection(dci_estimation,tmp_e,L*108,n_rnti, NR_POLAR_DCI_MESSAGE_TYPE, dci_length, L);
+        LOG_D(NR_PHY_DCI,
+              "(%i.%i) Received dci indication (rnti %x,dci format %d,n_CCE %d,payloadSize %d,payload %llx)\n",
+              proc->frame_rx,
+              proc->nr_slot_rx,
+              n_rnti,
+              rel15->dci_format_options[k],
+              CCEind,
+              dci_length,
+              *(unsigned long long *)dci_estimation);
+        uint16_t mb = nr_dci_false_detection(dci_estimation, tmp_e, L * 108, n_rnti, NR_POLAR_DCI_MESSAGE_TYPE, dci_length, L);
         ue->dci_thres = (ue->dci_thres + mb) / 2;
-        if (mb > (ue->dci_thres+30)) {
+        if (mb > (ue->dci_thres + 30)) {
           LOG_W(NR_PHY_DCI,
                 "DCI false positive. Dropping DCI index %d. Mismatched bits: %d/%d. Current DCI threshold: %d\n",
                 j,

openair2/LAYER2/NR_MAC_UE/nr_ue_dci_configuration.c
@@ -107,7 +107,6 @@ NR_ControlResourceSet_t *ue_get_coreset(const NR_BWP_PDCCH_t *config, const int 
   return coreset;
 }
 
-
 void config_dci_pdu(NR_UE_MAC_INST_t *mac,
                     fapi_nr_dl_config_request_t *dl_config,
                     const int rnti_type,
@@ -160,36 +159,40 @@ void config_dci_pdu(NR_UE_MAC_INST_t *mac,
     rel15->coreset.pdcch_dmrs_scrambling_id = mac->physCellId;
   }
 
-  rel15->num_dci_options = (mac->ra.ra_state == nrRA_WAIT_RAR || rnti_type == TYPE_SI_RNTI_) ? 1 : 2;
-
+  int temp_num_dci_options = (mac->ra.ra_state == nrRA_WAIT_RAR || rnti_type == TYPE_SI_RNTI_) ? 1 : 2;
+  int dci_format[2] = {0};
   if (ss->searchSpaceType->present == NR_SearchSpace__searchSpaceType_PR_ue_Specific) {
     if (ss->searchSpaceType->choice.ue_Specific->dci_Formats ==
         NR_SearchSpace__searchSpaceType__ue_Specific__dci_Formats_formats0_0_And_1_0) {
-      rel15->dci_format_options[0] = NR_DL_DCI_FORMAT_1_0;
-      rel15->dci_format_options[1] = NR_UL_DCI_FORMAT_0_0;
+      dci_format[0] = NR_DL_DCI_FORMAT_1_0;
+      dci_format[1] = NR_UL_DCI_FORMAT_0_0;
     }
     else {
-      rel15->dci_format_options[0] = NR_DL_DCI_FORMAT_1_1;
-      rel15->dci_format_options[1] = NR_UL_DCI_FORMAT_0_1;
+      dci_format[0] = NR_DL_DCI_FORMAT_1_1;
+      dci_format[1] = NR_UL_DCI_FORMAT_0_1;
     }
   }
   else { // common
     AssertFatal(ss->searchSpaceType->choice.common->dci_Format0_0_AndFormat1_0,
                 "Only supporting format 10 and 00 for common SS\n");
-    rel15->dci_format_options[0] = NR_DL_DCI_FORMAT_1_0;
-    rel15->dci_format_options[1] = NR_UL_DCI_FORMAT_0_0;
+    dci_format[0] = NR_DL_DCI_FORMAT_1_0;
+    dci_format[1] = NR_UL_DCI_FORMAT_0_0;
   }
 
   NR_UE_ServingCell_Info_t *sc_info = &mac->sc_info;
-  // loop over RNTI type and configure resource allocation for DCI
-  for (int i = 0; i < rel15->num_dci_options; i++) {
+  // loop over DCI options and configure resource allocation
+  // need to configure mac->def_dci_pdu_rel15 for all possible format options
+  for (int i = 0; i < temp_num_dci_options; i++) {
     rel15->ss_type_options[i] = ss->searchSpaceType->present;
-    const int dci_format = rel15->dci_format_options[i];
+    if (dci_format[i] == NR_DL_DCI_FORMAT_1_0 || dci_format[i] == NR_UL_DCI_FORMAT_0_0)
+      rel15->dci_format_options[i] = NFAPI_NR_FORMAT_0_0_AND_1_0;
+    else
+      rel15->dci_format_options[i] = NFAPI_NR_FORMAT_0_1_AND_1_1;
     uint16_t alt_size = 0;
     if(current_DL_BWP) {
-      // computing alternative size for padding
+      // computing alternative size for padding or truncation
       dci_pdu_rel15_t temp_pdu;
-      if(dci_format == NR_DL_DCI_FORMAT_1_0)
+      if (dci_format[i] == NR_DL_DCI_FORMAT_1_0)
         alt_size = nr_dci_size(current_DL_BWP,
                                current_UL_BWP,
                                sc_info,
@@ -202,7 +205,7 @@ void config_dci_pdu(NR_UE_MAC_INST_t *mac,
                                ss->searchSpaceType->present,
                                mac->type0_PDCCH_CSS_config.num_rbs,
                                0);
-      if(dci_format == NR_UL_DCI_FORMAT_0_0)
+      if (dci_format[i] == NR_UL_DCI_FORMAT_0_0)
         alt_size = nr_dci_size(current_DL_BWP,
                                current_UL_BWP,
                                sc_info,
@@ -221,8 +224,8 @@ void config_dci_pdu(NR_UE_MAC_INST_t *mac,
                                     current_UL_BWP,
                                     sc_info,
                                     mac->pdsch_HARQ_ACK_Codebook,
-                                    &mac->def_dci_pdu_rel15[dl_config->slot][dci_format],
-                                    dci_format,
+                                    &mac->def_dci_pdu_rel15[dl_config->slot][dci_format[i]],
+                                    dci_format[i],
                                     rnti_type,
                                     coreset,
                                     dl_bwp_id,
@@ -233,6 +236,13 @@ void config_dci_pdu(NR_UE_MAC_INST_t *mac,
       return;
     rel15->dci_length_options[i] = dci_size;
   }
+
+  // DCI 0_0 and 1_0 are same size, L1 just needs to look for 1 option
+  // L2 decides format based on format indicator in payload
+  if (rel15->dci_format_options[0] == NFAPI_NR_FORMAT_0_0_AND_1_0)
+    rel15->num_dci_options = 1;
+  else
+    rel15->num_dci_options = 2;
 
   rel15->BWPStart = coreset_id == 0 ? mac->type0_PDCCH_CSS_config.cset_start_rb : current_DL_BWP->BWPStart;
   rel15->BWPSize = coreset_id == 0 ? mac->type0_PDCCH_CSS_config.num_rbs : current_DL_BWP->BWPSize;