chinchans/oai5g

Add utility functions related to ERROR.indication

nfapi/open-nFAPI/fapi/inc/nr_fapi_p5_utils.h
@@ -45,6 +45,7 @@ bool compare_start_request(const nfapi_nr_start_request_scf_t *unpacked_req, con
 bool compare_start_response(const nfapi_nr_start_response_scf_t *unpacked_req, const nfapi_nr_start_response_scf_t *req);
 bool compare_stop_request(const nfapi_nr_stop_request_scf_t *unpacked_req, const nfapi_nr_stop_request_scf_t *req);
 bool compare_stop_indication(const nfapi_nr_stop_indication_scf_t *unpacked_req, const nfapi_nr_stop_indication_scf_t *req);
+bool compare_error_indication(const nfapi_nr_error_indication_scf_t *unpacked_req, const nfapi_nr_error_indication_scf_t *req);
 
 void free_param_request(nfapi_nr_param_request_scf_t *msg);
 void free_param_response(nfapi_nr_param_response_scf_t *msg);
@@ -54,6 +55,7 @@ void free_start_request(nfapi_nr_start_request_scf_t *msg);
 void free_start_response(nfapi_nr_start_response_scf_t *msg);
 void free_stop_request(nfapi_nr_stop_request_scf_t *msg);
 void free_stop_indication(nfapi_nr_stop_indication_scf_t *msg);
+void free_error_indication(nfapi_nr_error_indication_scf_t *msg);
 
 void copy_param_request(const nfapi_nr_param_request_scf_t *src,  nfapi_nr_param_request_scf_t *dst);
 void copy_param_response(const nfapi_nr_param_response_scf_t *src,  nfapi_nr_param_response_scf_t *dst);
@@ -63,5 +65,6 @@ void copy_start_request(const nfapi_nr_start_request_scf_t *src,  nfapi_nr_start
 void copy_start_response(const nfapi_nr_start_response_scf_t *src,  nfapi_nr_start_response_scf_t *dst);
 void copy_stop_request(const nfapi_nr_stop_request_scf_t *src,  nfapi_nr_stop_request_scf_t *dst);
 void copy_stop_indication(const nfapi_nr_stop_indication_scf_t *src,  nfapi_nr_stop_indication_scf_t *dst);
+void copy_error_indication(const nfapi_nr_error_indication_scf_t *src,  nfapi_nr_error_indication_scf_t *dst);
 
 #endif // OPENAIRINTERFACE_NR_FAPI_P5_UTILS_H

Nyanm/oai-ran-tlab

nfapi/open-nFAPI/fapi/inc/nr_fapi_p5_utils.h
@@ -71,6 +71,7 @@ bool eq_start_request(const nfapi_nr_start_request_scf_t *unpacked_req, const nf
 bool eq_start_response(const nfapi_nr_start_response_scf_t *unpacked_req, const nfapi_nr_start_response_scf_t *req);
 bool eq_stop_request(const nfapi_nr_stop_request_scf_t *unpacked_req, const nfapi_nr_stop_request_scf_t *req);
 bool eq_stop_indication(const nfapi_nr_stop_indication_scf_t *unpacked_req, const nfapi_nr_stop_indication_scf_t *req);
+bool eq_error_indication(const nfapi_nr_error_indication_scf_t *unpacked_req, const nfapi_nr_error_indication_scf_t *req);
 
 void free_param_request(nfapi_nr_param_request_scf_t *msg);
 void free_param_response(nfapi_nr_param_response_scf_t *msg);
@@ -80,6 +81,7 @@ void free_start_request(nfapi_nr_start_request_scf_t *msg);
 void free_start_response(nfapi_nr_start_response_scf_t *msg);
 void free_stop_request(nfapi_nr_stop_request_scf_t *msg);
 void free_stop_indication(nfapi_nr_stop_indication_scf_t *msg);
+void free_error_indication(nfapi_nr_error_indication_scf_t *msg);
 
 void copy_param_request(const nfapi_nr_param_request_scf_t *src, nfapi_nr_param_request_scf_t *dst);
 void copy_param_response(const nfapi_nr_param_response_scf_t *src, nfapi_nr_param_response_scf_t *dst);
@@ -89,5 +91,6 @@ void copy_start_request(const nfapi_nr_start_request_scf_t *src, nfapi_nr_start_
 void copy_start_response(const nfapi_nr_start_response_scf_t *src, nfapi_nr_start_response_scf_t *dst);
 void copy_stop_request(const nfapi_nr_stop_request_scf_t *src, nfapi_nr_stop_request_scf_t *dst);
 void copy_stop_indication(const nfapi_nr_stop_indication_scf_t *src, nfapi_nr_stop_indication_scf_t *dst);
+void copy_error_indication(const nfapi_nr_error_indication_scf_t *src, nfapi_nr_error_indication_scf_t *dst);
 
 #endif // OPENAIRINTERFACE_NR_FAPI_P5_UTILS_H