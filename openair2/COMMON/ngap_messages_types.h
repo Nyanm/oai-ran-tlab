turkeryilmaz/openairinterface5g

Add guami_t to ngran_types.h
* refactor NGAP and RRC code to use one common GUAMI type
  instead of two identical ones

openair2/COMMON/ngap_messages_types.h
@@ -205,15 +205,6 @@ typedef struct pdusession_level_qos_parameter_s {
   ngap_allocation_retention_priority_t allocation_retention_priority;
 } pdusession_level_qos_parameter_t;
 
-typedef struct ngap_guami_s {
-  uint16_t mcc;
-  uint16_t mnc;
-  uint8_t  mnc_len;
-  uint8_t  amf_region_id;
-  uint16_t amf_set_id;
-  uint8_t  amf_pointer;
-} ngap_guami_t;
-
 typedef struct fiveg_s_tmsi_s {
   uint16_t amf_set_id;
   uint8_t  amf_pointer;
@@ -239,7 +230,7 @@ typedef enum ngap_ue_identities_presenceMask_e {
 typedef struct ngap_ue_identity_s {
   ngap_ue_identities_presenceMask_t presenceMask;
   fiveg_s_tmsi_t  s_tmsi;
-  ngap_guami_t    guami;
+  guami_t guami;
 } ngap_ue_identity_t;
 
 typedef struct ngap_nas_pdu_s {
@@ -614,7 +605,7 @@ typedef struct ngap_initial_context_setup_req_s {
   ngap_ambr_t ue_ambr;
 
   /* guami */
-  ngap_guami_t guami;
+  guami_t guami;
 
   /* allowed nssai */
   uint8_t nb_allowed_nssais;
@@ -720,7 +711,7 @@ typedef struct ngap_path_switch_req_s {
   /* AMF UE id  */
   uint64_t amf_ue_ngap_id;
 
-  ngap_guami_t ue_guami;
+  guami_t ue_guami;
 
   uint16_t ue_initial_id;
   /* Security algorithms */

openair2/RRC/NR/nr_rrc_defs.h
@@ -267,7 +267,8 @@ typedef struct gNB_RRC_UE_s {
 
   uint32_t                           rrc_ue_id;
   uint64_t amf_ue_ngap_id;
-  nr_rrc_guami_t                     ue_guami;
+  // Globally Unique AMF Identifier
+  guami_t ue_guami;
 
   ngap_security_capabilities_t       security_capabilities;
   //NSA block

openair3/NGAP/ngap_gNB_nnsf.c
@@ -238,10 +238,9 @@ ngap_gNB_nnsf_select_amf_by_amf_setid(ngap_gNB_instance_t       *instance_p,
   return NULL;
 }
 
-struct ngap_gNB_amf_data_s *
-ngap_gNB_nnsf_select_amf_by_guami(ngap_gNB_instance_t       *instance_p,
-                                   ngap_rrc_establishment_cause_t  cause,
-                                   ngap_guami_t                   guami)
+struct ngap_gNB_amf_data_s *ngap_gNB_nnsf_select_amf_by_guami(ngap_gNB_instance_t *instance_p,
+                                                              ngap_rrc_establishment_cause_t cause,
+                                                              guami_t guami)
 {
   struct ngap_gNB_amf_data_s *amf_data_p             = NULL;
 
@@ -331,10 +330,7 @@ ngap_gNB_nnsf_select_amf_by_guami(ngap_gNB_instance_t       *instance_p,
   return NULL;
 }
 
-
-struct ngap_gNB_amf_data_s *
-ngap_gNB_nnsf_select_amf_by_guami_no_cause(ngap_gNB_instance_t       *instance_p,
-                                            ngap_guami_t                   guami)
+struct ngap_gNB_amf_data_s *ngap_gNB_nnsf_select_amf_by_guami_no_cause(ngap_gNB_instance_t *instance_p, guami_t guami)
 {
   struct ngap_gNB_amf_data_s *amf_data_p             = NULL;
   struct ngap_gNB_amf_data_s *amf_highest_capacity_p = NULL;

openair3/NGAP/ngap_gNB_nnsf.h
@@ -45,13 +45,10 @@ ngap_gNB_nnsf_select_amf_by_amf_setid(ngap_gNB_instance_t       *instance_p,
                                      int                        selected_plmn_identity,
                                      uint8_t                    amf_setid);
 
-struct ngap_gNB_amf_data_s*
-ngap_gNB_nnsf_select_amf_by_guami(ngap_gNB_instance_t       *instance_p,
-                                   ngap_rrc_establishment_cause_t  cause,
-                                   ngap_guami_t                   guami);
+struct ngap_gNB_amf_data_s *ngap_gNB_nnsf_select_amf_by_guami(ngap_gNB_instance_t *instance_p,
+                                                              ngap_rrc_establishment_cause_t cause,
+                                                              guami_t guami);
 
-struct ngap_gNB_amf_data_s*
-ngap_gNB_nnsf_select_amf_by_guami_no_cause(ngap_gNB_instance_t       *instance_p,
-                                   ngap_guami_t                   guami);
+struct ngap_gNB_amf_data_s *ngap_gNB_nnsf_select_amf_by_guami_no_cause(ngap_gNB_instance_t *instance_p, guami_t guami);
 
 #endif /* NGAP_GNB_NNSF_H_ */

Nyanm/oai-ran-tlab

openair2/COMMON/ngap_messages_types.h
@@ -199,15 +199,6 @@ typedef struct pdusession_level_qos_parameter_s {
   ngap_allocation_retention_priority_t allocation_retention_priority;
 } pdusession_level_qos_parameter_t;
 
-typedef struct ngap_guami_s {
-  uint16_t mcc;
-  uint16_t mnc;
-  uint8_t  mnc_len;
-  uint8_t  amf_region_id;
-  uint16_t amf_set_id;
-  uint8_t  amf_pointer;
-} ngap_guami_t;
-
 typedef struct fiveg_s_tmsi_s {
   uint16_t amf_set_id;
   uint8_t  amf_pointer;
@@ -233,7 +224,7 @@ typedef enum ngap_ue_identities_presenceMask_e {
 typedef struct ngap_ue_identity_s {
   ngap_ue_identities_presenceMask_t presenceMask;
   fiveg_s_tmsi_t  s_tmsi;
-  ngap_guami_t    guami;
+  nr_guami_t guami;
 } ngap_ue_identity_t;
 
 typedef struct ngap_nas_pdu_s {
@@ -613,7 +604,7 @@ typedef struct ngap_initial_context_setup_req_s {
   ngap_ambr_t ue_ambr;
 
   /* guami */
-  ngap_guami_t guami;
+  nr_guami_t guami;
 
   /* allowed nssai */
   uint8_t nb_allowed_nssais;
@@ -719,7 +710,7 @@ typedef struct ngap_path_switch_req_s {
   /* AMF UE id  */
   uint64_t amf_ue_ngap_id;
 
-  ngap_guami_t ue_guami;
+  nr_guami_t ue_guami;
 
   uint16_t ue_initial_id;
   /* Security algorithms */

openair2/RRC/NR/nr_rrc_defs.h
@@ -86,15 +86,6 @@ typedef struct nr_e_rab_param_s {
   uint8_t xid; // transaction_id
 } __attribute__ ((__packed__)) nr_e_rab_param_t;
 
-typedef struct nr_rrc_guami_s {
-  uint16_t mcc;
-  uint16_t mnc;
-  uint8_t  mnc_len;
-  uint8_t  amf_region_id;
-  uint16_t amf_set_id;
-  uint8_t  amf_pointer;
-} nr_rrc_guami_t;
-
 typedef enum pdu_session_satus_e {
   PDU_SESSION_STATUS_NEW,
   PDU_SESSION_STATUS_DONE,
@@ -217,7 +208,8 @@ typedef struct gNB_RRC_UE_s {
   uint64_t nr_cellid;
   uint32_t                           rrc_ue_id;
   uint64_t amf_ue_ngap_id;
-  nr_rrc_guami_t                     ue_guami;
+  // Globally Unique AMF Identifier
+  nr_guami_t ue_guami;
 
   ngap_security_capabilities_t       security_capabilities;
   //NSA block

openair3/NGAP/ngap_gNB_nnsf.c
@@ -234,10 +234,9 @@ ngap_gNB_nnsf_select_amf_by_amf_setid(ngap_gNB_instance_t       *instance_p,
   return NULL;
 }
 
-struct ngap_gNB_amf_data_s *
-ngap_gNB_nnsf_select_amf_by_guami(ngap_gNB_instance_t       *instance_p,
-                                   ngap_rrc_establishment_cause_t  cause,
-                                   ngap_guami_t                   guami)
+struct ngap_gNB_amf_data_s *ngap_gNB_nnsf_select_amf_by_guami(ngap_gNB_instance_t *instance_p,
+                                                              ngap_rrc_establishment_cause_t cause,
+                                                              nr_guami_t guami)
 {
   struct ngap_gNB_amf_data_s *amf_data_p             = NULL;
 
@@ -327,10 +326,7 @@ ngap_gNB_nnsf_select_amf_by_guami(ngap_gNB_instance_t       *instance_p,
   return NULL;
 }
 
-
-struct ngap_gNB_amf_data_s *
-ngap_gNB_nnsf_select_amf_by_guami_no_cause(ngap_gNB_instance_t       *instance_p,
-                                            ngap_guami_t                   guami)
+struct ngap_gNB_amf_data_s *ngap_gNB_nnsf_select_amf_by_guami_no_cause(ngap_gNB_instance_t *instance_p, nr_guami_t guami)
 {
   struct ngap_gNB_amf_data_s *amf_data_p             = NULL;
   struct ngap_gNB_amf_data_s *amf_highest_capacity_p = NULL;

openair3/NGAP/ngap_gNB_nnsf.h
@@ -49,13 +49,10 @@ ngap_gNB_nnsf_select_amf_by_amf_setid(ngap_gNB_instance_t       *instance_p,
                                      int                        selected_plmn_identity,
                                      uint8_t                    amf_setid);
 
-struct ngap_gNB_amf_data_s*
-ngap_gNB_nnsf_select_amf_by_guami(ngap_gNB_instance_t       *instance_p,
-                                   ngap_rrc_establishment_cause_t  cause,
-                                   ngap_guami_t                   guami);
+struct ngap_gNB_amf_data_s *ngap_gNB_nnsf_select_amf_by_guami(ngap_gNB_instance_t *instance_p,
+                                                              ngap_rrc_establishment_cause_t cause,
+                                                              nr_guami_t guami);
 
-struct ngap_gNB_amf_data_s*
-ngap_gNB_nnsf_select_amf_by_guami_no_cause(ngap_gNB_instance_t       *instance_p,
-                                   ngap_guami_t                   guami);
+struct ngap_gNB_amf_data_s *ngap_gNB_nnsf_select_amf_by_guami_no_cause(ngap_gNB_instance_t *instance_p, nr_guami_t guami);
 
 #endif /* NGAP_GNB_NNSF_H_ */