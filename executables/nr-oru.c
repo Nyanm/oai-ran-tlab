233066ba05d6aa1cda9f16c4275a6d375acffa2f
Bartosz Podrygajlo <bartosz.podrygajlo@openairinterface.org>
2025-09-12 10:03:02 +0200

    DL FH processing for O-RU
    Add DFT and symbol rotation to O-RU

--- executables/nr-oru.c
+++ executables/nr-oru.c
@@ -26,6 +26,36 @@
 
 #include <sched.h>
 
+void oru_downlink_processing(RU_t *ru, c16_t* txDataF_ptr[ru->nb_tx], int frame, int slot, int start_symbol, int num_symbols) {
+  start_meas(&ru->tx_fhaul);
+  NR_DL_FRAME_PARMS *fp = ru->nr_frame_parms;
+  for (int aatx = 0; aatx < ru->nb_tx; aatx++) {
+    apply_nr_rotation_TX(fp,
+                         txDataF_ptr[aatx],
+                         fp->symbol_rotation[0],
+                         slot,
+                         fp->N_RB_DL,
+                         start_symbol,
+                         num_symbols);
+    nr_feptx0(ru, slot, start_symbol, num_symbols, aatx);
+  }
+  // Assume this function called in order
+  static int frame_tx_unwrap = 0;
+  static int last_frame = 0;
+  if (frame < last_frame) {
+    frame_tx_unwrap += 1024;
+  }
+  last_frame = frame;
+
+  uint64_t timestamp_tx =
+      (uint64_t)(frame + frame_tx_unwrap) * fp->samples_per_subframe * 10 + fp->get_samples_slot_timestamp(slot, fp, 0);
+
+  // TODO: prepare tx here
+  // tx_rf(ru, frame, slot, timestamp_tx);
+  (void)timestamp_tx;
+  stop_meas(&ru->tx_fhaul);
+}
+
 void *oru_north_read_thread(void *arg)
 {
   ORU_t *oru = (ORU_t *)arg;
@@ -36,7 +66,7 @@ void *oru_north_read_thread(void *arg)
   sprintf(threadname, "oru_thread %u", ru->idx);
 
   AssertFatal(ru->ifdevice.xran_api.north_in_func != NULL, "No fronthaul interface at north port");
-  __attribute__((aligned(32))) c16_t txDataF[ru->nb_tx][ceil_mod(fp->ofdm_symbol_size * 7, 32)];
+  __attribute__((aligned(32))) c16_t txDataF[ru->nb_tx][ceil_mod(fp->ofdm_symbol_size * 14, 32)];
   c16_t *txDataF_ptr[ru->nb_tx];
   for (int aatx = 0; aatx < ru->nb_tx; aatx++) {
     txDataF_ptr[aatx] = txDataF[aatx];
@@ -45,13 +75,13 @@ void *oru_north_read_thread(void *arg)
     int num_symbols = 0;
     sense_of_time_t sense_of_time;
     ru->ifdevice.xran_api.north_in_func((uint32_t **)txDataF_ptr, ru->nb_tx, &sense_of_time, &num_symbols);
-    LOG_I(PHY,
+    LOG_D(PHY,
           "[RU_thread] read data: frame %d, slot %d, start_symbol %d, num_symbols %d\n",
           sense_of_time.frame,
           sense_of_time.slot,
           sense_of_time.symbol,
           num_symbols);
-    // TODO: Trigger TX. Use FIFOs / Actors
+    oru_downlink_processing(ru, txDataF_ptr, sense_of_time.frame, sense_of_time.slot, sense_of_time.symbol, num_symbols);
   }
   return NULL;
 }
