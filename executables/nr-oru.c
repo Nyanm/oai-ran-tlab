684dd38dcaacb8dd1e2bd97207aa939a908adb18
Bartosz Podrygajlo <bartosz.podrygajlo@openairinterface.org>
2025-09-15 11:24:45 +0200

    Use synchronized rfdevice to xran timestamps in ORU
    Synchronize the southbound (time domain) interface to XRAN timestamps
    in ORU. This currently only works with vrtsim.
    
    Transmit the samples after processing in oru_downlink_processing.

--- executables/nr-oru.c
+++ executables/nr-oru.c
@@ -26,7 +26,63 @@
 
 #include <sched.h>
 
-void oru_downlink_processing(RU_t *ru, c16_t *txDataF_ptr[ru->nb_tx], int frame, int slot, int start_symbol, int num_symbols)
+typedef struct {
+  openair0_timestamp sample;
+  int slot;
+  int frame;
+  int symbol;
+} initial_sync_t;
+
+typedef struct {
+  int frame_unwrap;
+  int last_frame;
+  int64_t sync_offset;
+  initial_sync_t initial_sync;
+} sync_params_t;
+
+extern void tx_rf(RU_t *ru, int frame, int slot, uint64_t timestamp);
+
+void perform_initial_sync(ORU_t *oru, sense_of_time_t *sense_of_time, sync_params_t *sync_params)
+{
+  initial_sync_t *initial_sync = &sync_params->initial_sync;
+  initial_sync->frame = sense_of_time->frame;
+  initial_sync->slot = sense_of_time->slot;
+  initial_sync->symbol = sense_of_time->symbol;
+  initial_sync->sample = oru->ru->rfdevice.get_timestamp(&oru->ru->rfdevice, &sense_of_time->ts);
+  NR_DL_FRAME_PARMS *fp = oru->ru->nr_frame_parms;
+  sync_params->frame_unwrap = 0;
+  sync_params->last_frame = initial_sync->frame;
+  sync_params->sync_offset = initial_sync->sample;
+  sync_params->sync_offset -= (uint64_t)(sync_params->initial_sync.frame) * fp->samples_per_subframe * 10
+                              + fp->get_samples_slot_timestamp(sync_params->initial_sync.slot, fp, 0);
+  LOG_I(PHY,
+        "RU synchronized: frame, slot %d.%d, symbol %d,  offset: %ld\n",
+        initial_sync->frame,
+        initial_sync->slot,
+        initial_sync->symbol,
+        sync_params->sync_offset);
+}
+
+openair0_timestamp get_timestamp(ORU_t *oru, sense_of_time_t *sense_of_time, sync_params_t *sync_params)
+{
+  if (sync_params->last_frame > sense_of_time->frame) {
+    sync_params->frame_unwrap++;
+  }
+  sync_params->last_frame = sense_of_time->frame;
+  NR_DL_FRAME_PARMS *fp = oru->ru->nr_frame_parms;
+  uint64_t timestamp = (uint64_t)(sense_of_time->frame + sync_params->frame_unwrap) * fp->samples_per_subframe * 10
+                       + fp->get_samples_slot_timestamp(sense_of_time->slot, fp, 0);
+  timestamp += sync_params->sync_offset;
+  return timestamp;
+}
+
+void oru_downlink_processing(RU_t *ru,
+                             c16_t *txDataF_ptr[ru->nb_tx],
+                             int frame,
+                             int slot,
+                             int start_symbol,
+                             int num_symbols,
+                             openair0_timestamp timestamp_tx)
 {
   start_meas(&ru->tx_fhaul);
   NR_DL_FRAME_PARMS *fp = ru->nr_frame_parms;
@@ -34,20 +90,7 @@ void oru_downlink_processing(RU_t *ru, c16_t *txDataF_ptr[ru->nb_tx], int frame,
     apply_nr_rotation_TX(fp, txDataF_ptr[aatx], fp->symbol_rotation[0], slot, fp->N_RB_DL, start_symbol, num_symbols);
     nr_feptx0(ru, slot, start_symbol, num_symbols, aatx);
   }
-  // Assume this function called in order
-  static int frame_tx_unwrap = 0;
-  static int last_frame = 0;
-  if (frame < last_frame) {
-    frame_tx_unwrap += 1024;
-  }
-  last_frame = frame;
-
-  uint64_t timestamp_tx =
-      (uint64_t)(frame + frame_tx_unwrap) * fp->samples_per_subframe * 10 + fp->get_samples_slot_timestamp(slot, fp, 0);
-
-  // TODO: prepare tx here
-  // tx_rf(ru, frame, slot, timestamp_tx);
-  (void)timestamp_tx;
+  tx_rf(ru, frame, slot, timestamp_tx);
   stop_meas(&ru->tx_fhaul);
 }
 
@@ -66,10 +109,23 @@ void *oru_north_read_thread(void *arg)
   for (int aatx = 0; aatx < ru->nb_tx; aatx++) {
     txDataF_ptr[aatx] = txDataF[aatx];
   }
+  sync_params_t sync_params;
+
   while (!oai_exit) {
     int num_symbols = 0;
     sense_of_time_t sense_of_time;
     ru->ifdevice.xran_api.north_in_func((uint32_t **)txDataF_ptr, ru->nb_tx, &sense_of_time, &num_symbols);
+    if (sense_of_time.symbol == 0) {
+      perform_initial_sync(oru, &sense_of_time, &sync_params);
+      break;
+    }
+  }
+
+  while (!oai_exit) {
+    int num_symbols = 0;
+    sense_of_time_t sense_of_time;
+    ru->ifdevice.xran_api.north_in_func((uint32_t **)txDataF_ptr, ru->nb_tx, &sense_of_time, &num_symbols);
+    openair0_timestamp timestamp_tx = get_timestamp(oru, &sense_of_time, &sync_params);
     if ((sense_of_time.frame & 0xff) == 0 && sense_of_time.slot == 0) {
       LOG_I(PHY,
             "[RU_thread] read data: frame %d, slot %d, start_symbol %d, num_symbols %d\n",
@@ -78,7 +134,13 @@ void *oru_north_read_thread(void *arg)
             sense_of_time.symbol,
             num_symbols);
     }
-    oru_downlink_processing(ru, txDataF_ptr, sense_of_time.frame, sense_of_time.slot, sense_of_time.symbol, num_symbols);
+    oru_downlink_processing(ru,
+                            txDataF_ptr,
+                            sense_of_time.frame,
+                            sense_of_time.slot,
+                            sense_of_time.symbol,
+                            num_symbols,
+                            timestamp_tx);
   }
   return NULL;
 }
