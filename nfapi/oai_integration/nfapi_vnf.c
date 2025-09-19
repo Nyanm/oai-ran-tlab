arandomdev/openairinterface5g

Use existing VNF functions for P7 messages
Remove duplicated functions in fapi_vnf_p7.c, leaving only functions for messages that require usage of the secondaty data_buf for unpacking

Move vnf_info struct declaration to nfapi_vnf.h to be available in fapi_nvIPC.c

nfapi/oai_integration/nfapi_vnf.c
@@ -32,7 +32,6 @@
 #include <arpa/inet.h>
 
 #include "nfapi_nr_interface_scf.h"
-#include "nfapi_vnf_interface.h"
 #include "nfapi_vnf.h"
 #include <vnf.h>
 #include <vnf_p7.h>
@@ -64,135 +63,15 @@
 
 #ifdef ENABLE_AERIAL
 #include "aerial/fapi_nvIPC.h"
+#include "aerial/fapi_vnf_p7.h"
 #include "nr_fapi_p5.h"
+#include "nr_fapi_p7.h"
 #endif
 
 #define TEST
 
 extern RAN_CONTEXT_t RC;
 extern UL_RCC_IND_t  UL_RCC_INFO;
-
-typedef struct {
-  uint8_t enabled;
-  uint32_t rx_port;
-  uint32_t tx_port;
-  char tx_addr[80];
-} udp_data;
-
-typedef struct {
-  uint16_t index;
-  uint16_t id;
-  uint8_t rfs[2];
-  uint8_t excluded_rfs[2];
-
-  udp_data udp;
-
-  char local_addr[80];
-  int local_port;
-
-  char *remote_addr;
-  int remote_port;
-
-  uint8_t duplex_mode;
-  uint16_t dl_channel_bw_support;
-  uint16_t ul_channel_bw_support;
-  uint8_t num_dl_layers_supported;
-  uint8_t num_ul_layers_supported;
-  uint16_t release_supported;
-  uint8_t nmm_modes_supported;
-
-  uint8_t dl_ues_per_subframe;
-  uint8_t ul_ues_per_subframe;
-
-  uint8_t first_subframe_ind;
-
-  // timing information recevied from the vnf
-  uint8_t timing_window;
-  uint8_t timing_info_mode;
-  uint8_t timing_info_period;
-
-} phy_info;
-
-typedef struct {
-  uint16_t index;
-  uint16_t band;
-  int16_t max_transmit_power;
-  int16_t min_transmit_power;
-  uint8_t num_antennas_supported;
-  uint32_t min_downlink_frequency;
-  uint32_t max_downlink_frequency;
-  uint32_t max_uplink_frequency;
-  uint32_t min_uplink_frequency;
-} rf_info;
-
-typedef struct {
-
-  int release;
-  phy_info phys[2];
-  rf_info rfs[2];
-
-  uint8_t sync_mode;
-  uint8_t location_mode;
-  uint8_t location_coordinates[6];
-  uint32_t dl_config_timing;
-  uint32_t ul_config_timing;
-  uint32_t tx_timing;
-  uint32_t hi_dci0_timing;
-
-  uint16_t max_phys;
-  uint16_t max_total_bw;
-  uint16_t max_total_dl_layers;
-  uint16_t max_total_ul_layers;
-  uint8_t shared_bands;
-  uint8_t shared_pa;
-  int16_t max_total_power;
-  uint8_t oui;
-
-  uint8_t wireshark_test_mode;
-
-} pnf_info;
-
-typedef struct mac mac_t;
-
-typedef struct mac {
-
-  void *user_data;
-
-  void (*dl_config_req)(mac_t *mac, nfapi_dl_config_request_t *req);
-  void (*ul_config_req)(mac_t *mac, nfapi_ul_config_request_t *req);
-  void (*hi_dci0_req)(mac_t *mac, nfapi_hi_dci0_request_t *req);
-  void (*tx_req)(mac_t *mac, nfapi_tx_request_t *req);
-} mac_t;
-
-typedef struct {
-
-  int local_port;
-  char local_addr[80];
-
-  unsigned timing_window;
-  unsigned periodic_timing_enabled;
-  unsigned aperiodic_timing_enabled;
-  unsigned periodic_timing_period;
-
-  // This is not really the right place if we have multiple PHY,
-  // should be part of the phy struct
-  udp_data udp;
-
-  uint8_t thread_started;
-
-  nfapi_vnf_p7_config_t *config;
-
-  mac_t *mac;
-
-} vnf_p7_info;
-
-typedef struct {
-
-  uint8_t wireshark_test_mode;
-  pnf_info pnfs[2];
-  vnf_p7_info p7_vnfs[2];
-
-} vnf_info;
 
 int vnf_pack_vendor_extension_tlv(void *ve, uint8_t **ppWritePackedMsg, uint8_t *end, nfapi_p4_p5_codec_config_t *codec) {
   //NFAPI_TRACE(NFAPI_TRACE_INFO, "vnf_pack_vendor_extension_tlv\n");
@@ -1455,9 +1334,7 @@ void *configure_nr_p7_vnf(void *ptr)
   p7_vnf->config->codec_config.deallocate = &vnf_deallocate;
   p7_vnf->config->allocate_p7_vendor_ext = &phy_nr_allocate_p7_vendor_ext;
   p7_vnf->config->deallocate_p7_vendor_ext = &phy_nr_deallocate_p7_vendor_ext;
-  p7_vnf->config->unpack_func = &nfapi_nr_p7_message_unpack;
-  p7_vnf->config->hdr_unpack_func = &nfapi_nr_p7_message_header_unpack;
-  p7_vnf->config->pack_func = &nfapi_nr_p7_message_pack;
+
 #ifdef ENABLE_WLS
   p7_vnf->config->unpack_func = &fapi_nr_p7_message_unpack;
   p7_vnf->config->hdr_unpack_func = &fapi_nr_p7_message_header_unpack;
@@ -1476,7 +1353,10 @@ void *configure_nr_p7_vnf(void *ptr)
 #endif
 
 #ifdef ENABLE_AERIAL
-
+  p7_vnf->config->unpack_func = &fapi_nr_p7_message_unpack;
+  p7_vnf->config->hdr_unpack_func = &fapi_nr_p7_message_header_unpack;
+  p7_vnf->config->pack_func = &fapi_nr_p7_message_pack;
+  p7_vnf->config->nr_pack_and_send_p7_msg = &fapi_nr_pack_and_send_p7_message;
 #endif
   return 0;
 }

Nyanm/oai-ran-tlab

nfapi/oai_integration/nfapi_vnf.c
@@ -32,7 +32,6 @@
 #include <arpa/inet.h>
 
 #include "nfapi_nr_interface_scf.h"
-#include "nfapi_vnf_interface.h"
 #include "nfapi_vnf.h"
 #include <vnf.h>
 #include <vnf_p7.h>
@@ -64,135 +63,15 @@
 
 #ifdef ENABLE_AERIAL
 #include "aerial/fapi_nvIPC.h"
+#include "aerial/fapi_vnf_p7.h"
 #include "nr_fapi_p5.h"
+#include "nr_fapi_p7.h"
 #endif
 
 #define TEST
 
 extern RAN_CONTEXT_t RC;
 extern UL_RCC_IND_t  UL_RCC_INFO;
-
-typedef struct {
-  uint8_t enabled;
-  uint32_t rx_port;
-  uint32_t tx_port;
-  char tx_addr[80];
-} udp_data;
-
-typedef struct {
-  uint16_t index;
-  uint16_t id;
-  uint8_t rfs[2];
-  uint8_t excluded_rfs[2];
-
-  udp_data udp;
-
-  char local_addr[80];
-  int local_port;
-
-  char *remote_addr;
-  int remote_port;
-
-  uint8_t duplex_mode;
-  uint16_t dl_channel_bw_support;
-  uint16_t ul_channel_bw_support;
-  uint8_t num_dl_layers_supported;
-  uint8_t num_ul_layers_supported;
-  uint16_t release_supported;
-  uint8_t nmm_modes_supported;
-
-  uint8_t dl_ues_per_subframe;
-  uint8_t ul_ues_per_subframe;
-
-  uint8_t first_subframe_ind;
-
-  // timing information recevied from the vnf
-  uint8_t timing_window;
-  uint8_t timing_info_mode;
-  uint8_t timing_info_period;
-
-} phy_info;
-
-typedef struct {
-  uint16_t index;
-  uint16_t band;
-  int16_t max_transmit_power;
-  int16_t min_transmit_power;
-  uint8_t num_antennas_supported;
-  uint32_t min_downlink_frequency;
-  uint32_t max_downlink_frequency;
-  uint32_t max_uplink_frequency;
-  uint32_t min_uplink_frequency;
-} rf_info;
-
-typedef struct {
-
-  int release;
-  phy_info phys[2];
-  rf_info rfs[2];
-
-  uint8_t sync_mode;
-  uint8_t location_mode;
-  uint8_t location_coordinates[6];
-  uint32_t dl_config_timing;
-  uint32_t ul_config_timing;
-  uint32_t tx_timing;
-  uint32_t hi_dci0_timing;
-
-  uint16_t max_phys;
-  uint16_t max_total_bw;
-  uint16_t max_total_dl_layers;
-  uint16_t max_total_ul_layers;
-  uint8_t shared_bands;
-  uint8_t shared_pa;
-  int16_t max_total_power;
-  uint8_t oui;
-
-  uint8_t wireshark_test_mode;
-
-} pnf_info;
-
-typedef struct mac mac_t;
-
-typedef struct mac {
-
-  void *user_data;
-
-  void (*dl_config_req)(mac_t *mac, nfapi_dl_config_request_t *req);
-  void (*ul_config_req)(mac_t *mac, nfapi_ul_config_request_t *req);
-  void (*hi_dci0_req)(mac_t *mac, nfapi_hi_dci0_request_t *req);
-  void (*tx_req)(mac_t *mac, nfapi_tx_request_t *req);
-} mac_t;
-
-typedef struct {
-
-  int local_port;
-  char local_addr[80];
-
-  unsigned timing_window;
-  unsigned periodic_timing_enabled;
-  unsigned aperiodic_timing_enabled;
-  unsigned periodic_timing_period;
-
-  // This is not really the right place if we have multiple PHY,
-  // should be part of the phy struct
-  udp_data udp;
-
-  uint8_t thread_started;
-
-  nfapi_vnf_p7_config_t *config;
-
-  mac_t *mac;
-
-} vnf_p7_info;
-
-typedef struct {
-
-  uint8_t wireshark_test_mode;
-  pnf_info pnfs[2];
-  vnf_p7_info p7_vnfs[2];
-
-} vnf_info;
 
 int vnf_pack_vendor_extension_tlv(void *ve, uint8_t **ppWritePackedMsg, uint8_t *end, nfapi_p4_p5_codec_config_t *codec) {
   //NFAPI_TRACE(NFAPI_TRACE_INFO, "vnf_pack_vendor_extension_tlv\n");
@@ -1455,9 +1334,7 @@ void *configure_nr_p7_vnf(void *ptr)
   p7_vnf->config->codec_config.deallocate = &vnf_deallocate;
   p7_vnf->config->allocate_p7_vendor_ext = &phy_nr_allocate_p7_vendor_ext;
   p7_vnf->config->deallocate_p7_vendor_ext = &phy_nr_deallocate_p7_vendor_ext;
-  p7_vnf->config->unpack_func = &nfapi_nr_p7_message_unpack;
-  p7_vnf->config->hdr_unpack_func = &nfapi_nr_p7_message_header_unpack;
-  p7_vnf->config->pack_func = &nfapi_nr_p7_message_pack;
+
 #ifdef ENABLE_WLS
   p7_vnf->config->unpack_func = &fapi_nr_p7_message_unpack;
   p7_vnf->config->hdr_unpack_func = &fapi_nr_p7_message_header_unpack;
@@ -1476,7 +1353,10 @@ void *configure_nr_p7_vnf(void *ptr)
 #endif
 
 #ifdef ENABLE_AERIAL
-
+  p7_vnf->config->unpack_func = &fapi_nr_p7_message_unpack;
+  p7_vnf->config->hdr_unpack_func = &fapi_nr_p7_message_header_unpack;
+  p7_vnf->config->pack_func = &fapi_nr_p7_message_pack;
+  p7_vnf->config->send_p7_msg = &aerial_nr_send_p7_message;
 #endif
   return 0;
 }