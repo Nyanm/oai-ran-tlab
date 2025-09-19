turkeryilmaz/openairinterface5g

Add new T2 Offload test

ci-scripts/main.py
@@ -251,10 +251,10 @@ def ExecuteActionWithParam(action, ctx):
 		workdir = CLUSTER.eNBSourceCodePath
 		success = cls_oaicitest.Deploy_Physim(ctx, HTML, node, workdir, script, options)
 
-	elif action == 'Deploy_Run_Docker_PhySim':
+	elif action == 'Deploy_Run_Docker_PhySim' or action == 'Deploy_Run_Source_PhySim':
 		node = test.findtext('node') or None
 		ctest_opt = test.findtext('ctest-opt') or ''
-		script = "scripts/docker-deploy-physims.sh"
+		script = "scripts/docker-deploy-physims.sh" if action == 'Deploy_Run_Docker_PhySim' else 'scripts/source-deploy-physims.sh'
 		options = f"{CONTAINERS.eNBSourceCodePath} {ctest_opt}"
 		workdir = CONTAINERS.eNBSourceCodePath
 		success = cls_oaicitest.Deploy_Physim(ctx, HTML, node, workdir, script, options)

Nyanm/oai-ran-tlab

ci-scripts/main.py
@@ -251,10 +251,10 @@ def ExecuteActionWithParam(action, ctx):
 		workdir = CLUSTER.eNBSourceCodePath
 		success = cls_oaicitest.Deploy_Physim(ctx, HTML, node, workdir, script, options)
 
-	elif action == 'Build_Deploy_Docker_PhySim':
+	elif action == 'Build_Deploy_Docker_PhySim' or action == 'Build_Deploy_Source_PhySim':
 		node = test.findtext('node') or None
 		ctest_opt = test.findtext('ctest-opt') or ''
-		script = "scripts/docker-build-and-deploy-physims.sh"
+		script = "scripts/docker-build-and-deploy-physims.sh" if action == 'Build_Deploy_Docker_PhySim' else 'scripts/source-deploy-physims.sh'
 		options = f"{CONTAINERS.eNBSourceCodePath} {ctest_opt}"
 		workdir = CONTAINERS.eNBSourceCodePath
 		success = cls_oaicitest.Deploy_Physim(ctx, HTML, node, workdir, script, options)