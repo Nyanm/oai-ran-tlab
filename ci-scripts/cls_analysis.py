turkeryilmaz/openairinterface5g

Pass CTest test description in custom property or environment
Prior to this change, each test would have, as label, it's description.
This leads to many labels, which are normally reserved for grouping
tests. Move the description into a custom property instead.

Since we rely on the JSON output for the generation of HTML reports, we
need to also extract the description from the generated JSON. However,
custom properties are only logged starting from cmake 3.30. For
back-wards compatibility, write an additional, separate environment
variable TEST_DESCRIPTION from which the description can be read as a
fallback.

For the add_timed_physim_test(), this has been already done in a parent
commit.

ci-scripts/cls_analysis.py
@@ -28,6 +28,23 @@ import json
 
 class Analysis():
 
+	def _get_test_description(properties):
+		env_vars = None
+		for p in properties:
+			if p["name"] == "ENVIRONMENT":
+				env_ars = p["value"] # save for later if no custom property
+			if p["name"] == "TEST_DESCRIPTION":
+				return p["value"]
+		# if we came till here, it means there is no custom test property
+		# saved in JSON.  See if we have a description in environment variables
+		if not env_vars:
+			return "<none>"
+		for ev in env_vars:
+			name, value = ev.split("=", 1)
+			if name == "TEST_DESCRIPTION":
+				return value
+		return "<none>"
+
 	def analyze_physim(result_junit, details_json, logPath):
 		try:
 			tree = ET.parse(result_junit)
@@ -60,7 +77,7 @@ class Analysis():
 		for test in root: # for each test
 			test_name = test.attrib["name"]
 			test_exec = json_test_desc[test_name]["properties"][1]["value"][0]
-			desc = json_test_desc[test_name]["properties"][1]["value"][1]
+			desc = Analysis._get_test_description(json_test_desc[test_name]["properties"])
 			# get runtime and checks
 			test_check = test.attrib["status"] == "run"
 			time = round(float(test.attrib["time"]), 1)

Nyanm/oai-ran-tlab

ci-scripts/cls_analysis.py
@@ -28,6 +28,23 @@ import json
 
 class Analysis():
 
+	def _get_test_description(properties):
+		env_vars = None
+		for p in properties:
+			if p["name"] == "ENVIRONMENT":
+				env_vars = p["value"] # save for later if no custom property
+			if p["name"] == "TEST_DESCRIPTION":
+				return p["value"]
+		# if we came till here, it means there is no custom test property
+		# saved in JSON.  See if we have a description in environment variables
+		if not env_vars:
+			return "<none>"
+		for ev in env_vars:
+			name, value = ev.split("=", 1)
+			if name == "TEST_DESCRIPTION":
+				return value
+		return "<none>"
+
 	def analyze_physim(result_junit, details_json, logPath):
 		try:
 			tree = ET.parse(result_junit)
@@ -60,7 +77,7 @@ class Analysis():
 		for test in root: # for each test
 			test_name = test.attrib["name"]
 			test_exec = json_test_desc[test_name]["properties"][1]["value"][0]
-			desc = json_test_desc[test_name]["properties"][1]["value"][1]
+			desc = Analysis._get_test_description(json_test_desc[test_name]["properties"])
 			# get runtime and checks
 			test_check = test.attrib["status"] == "run"
 			time = round(float(test.attrib["time"]), 1)