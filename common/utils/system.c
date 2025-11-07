eae421cce6116be5a21331eed13ab3fb822fb4e8
Bartosz Podrygajlo <bartosz.podrygajlo@openairinterface.org>
2025-11-07 12:35:45 +0100

    Update threadCreate
    Two modifications for threadCreate:
     - if -1 is passed, the thread no longer inherits its parent affinity.
       Instead thread affinity of the process is used.
     - if the name is too long, the thread name is shortened to maximum
       pthread name (16 characters).

--- common/utils/system.c
+++ common/utils/system.c
@@ -282,14 +282,25 @@ void threadCreate(pthread_t* t, void * (*func)(void*), void * param, char* name,
 
   ret=pthread_create(t, &attr, func, param);
   AssertFatal(ret == 0, "Error in pthread_create(): ret: %d, errno: %d\n", ret, errno);
-  
-  pthread_setname_np(*t, name);
-  if (affinity != -1 ) {
+
+  char short_name[16];
+  strncpy(short_name, name, sizeof(short_name) - 1);
+  short_name[sizeof(short_name) - 1] = '\0';
+  ret = pthread_setname_np(*t, short_name);
+  AssertFatal(ret == 0, "Error in pthread_setname_np(): ret: %d, errno: %d\n", ret, errno);
+
+  if (affinity != -1) {
     cpu_set_t cpuset;
     CPU_ZERO(&cpuset);
     CPU_SET(affinity, &cpuset);
     ret = pthread_setaffinity_np(*t, sizeof(cpu_set_t), &cpuset);
     AssertFatal(ret == 0, "Error in pthread_getaffinity_np(): ret: %d, errno: %d", ret, errno);
+  } else {
+    cpu_set_t cpuset;
+    ret = sched_getaffinity(0, sizeof(cpu_set_t), &cpuset);
+    AssertFatal(ret == 0, "Error in sched_getaffinity(): ret: %d, errno: %d", ret, errno);
+    ret = pthread_setaffinity_np(*t, sizeof(cpu_set_t), &cpuset);
+    AssertFatal(ret == 0, "Error in pthread_setaffinity_np(): ret: %d, errno: %d", ret, errno);
   }
   pthread_attr_destroy(&attr);
 }
