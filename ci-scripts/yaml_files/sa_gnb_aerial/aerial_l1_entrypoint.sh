f461f17b202dd77cf63aa068d4b120427815d3e7
Fredrik Molander <fmolander@nvidia.com>
2025-11-21 15:39:53 +0100

    updates for 25-3

--- ci-scripts/yaml_files/sa_gnb_aerial/aerial_l1_entrypoint.sh
+++ ci-scripts/yaml_files/sa_gnb_aerial/aerial_l1_entrypoint.sh
@@ -3,9 +3,8 @@
 # Check if cuBB_SDK is defined, if not, use default path
 cuBB_Path="${cuBB_SDK:-/opt/nvidia/cuBB}"
 
+#./insmod.sh
 cd "$cuBB_Path" || exit 1
-# Add gdrcopy to LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/usr/local/nvidia/lib:/usr/local/nvidia/lib64:/opt/mellanox/dpdk/lib/x86_64-linux-gnu:/opt/mellanox/doca/lib/x86_64-linux-gnu:/opt/nvidia/cuBB/cuPHY-CP/external/gdrcopy/build/x86_64/
 
 # Restart MPS
 # Export variables
@@ -14,7 +13,7 @@ export CUDA_MPS_PIPE_DIRECTORY=/var
 export CUDA_MPS_LOG_DIRECTORY=/var
 
 # Stop existing MPS
-sudo -E echo quit | sudo -E nvidia-cuda-mps-control
+echo quit | sudo -E nvidia-cuda-mps-control
 
 # Start MPS
 sudo -E nvidia-cuda-mps-control -d
@@ -24,11 +23,40 @@ sudo -E echo start_server -uid 0 | sudo -E nvidia-cuda-mps-control
 # Check if an argument is provided
 if [ $# -eq 0 ]; then
     # No argument provided, use default value
-    argument="P5G_FXN_GH"
+    serverVendorAndModel=$(cat /sys/devices/virtual/dmi/id/board_vendor)
+    serverVendorAndModel+="-"
+    serverVendorAndModel+=$(cat /sys/devices/virtual/dmi/id/board_name)
+    echo $serverVendorAndModel
+    case $serverVendorAndModel in
+	"Dell Inc.-06V45N")
+    	   argument="P5G_FXN_R750"
+	   ;;
+	"GIGABYTE-MU71-SU0-00")
+    	   argument="P5G_FXN"
+	   ;;
+        "Supermicro-G1SMH-G")
+    	   argument="P5G_WNC_GH"
+	   ;;
+	*)
+	   echo "Unrecognized server: $serverVendorAndModel"
+	   exit
+	   ;;
+    esac
 else
     # Argument provided, use it
     argument="$1"
 fi
+configFile=${cuBB_SDK}/cuPHY-CP/cuphycontroller/config/cuphycontroller_${argument}.yaml  
 
-sudo -E "$cuBB_Path"/build/cuPHY-CP/cuphycontroller/examples/cuphycontroller_scf "$argument"
-sudo -E ./build/cuPHY-CP/gt_common_libs/nvIPC/tests/pcap/pcap_collect
+#Change this to the MAC address of the ORU
+sudo -E sed -i "s/ dst_mac_addr:.*/ dst_mac_addr: e8:c7:cf:ac:58:3e/" ${configFile}  
+
+export AERIAL_LOG_PATH=/var/log/aerial
+sudo -E "$cuBB_Path"/build/cuPHY-CP/cuphycontroller/examples/cuphycontroller_scf $argument
+sudo -E ./build/cuPHY-CP/gt_common_libs/nvIPC/tests/pcap/pcap_collect nvipc /tmp
+sudo -E mv /tmp/nvipc*.pcap $AERIAL_LOG_PATH
+
+#Uncomment this if using multiple nvipc interfaces
+#sudo ./build/cuPHY-CP/gt_common_libs/nvIPC/tests/pcap/pcap_collect nvipc1 /tmp
+#sudo mv /tmp/nvipc*.pcap /var/log/aerial/
+#sleep infinity
