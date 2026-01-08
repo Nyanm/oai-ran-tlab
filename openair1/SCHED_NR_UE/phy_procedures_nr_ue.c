df7fbc233123ef246b09f0d7f95254f05c162e2a
Sakthivel Velumani <mail@sakthi.me>
2026-01-08 02:51:41 +0000

    refactor: make rxdataF_comp 4D array
    put symbol index as first dimenstion.

--- openair1/SCHED_NR_UE/phy_procedures_nr_ue.c
+++ openair1/SCHED_NR_UE/phy_procedures_nr_ue.c
@@ -628,12 +628,13 @@ static int nr_ue_pdsch_procedures(PHY_VARS_NR_UE *ue,
 
     const uint32_t rx_size_symbol = (freq_alloc.num_rbs * NR_NB_SC_PER_RB + 15) & ~15;
     fourDimArray_t *toFree2 = NULL;
-    allocCast3D(rxdataF_comp,
+    allocCast4D(rxdataF_comp,
                 int32_t,
                 toFree2,
+                NR_SYMBOLS_PER_SLOT,
                 dlsch[0].Nl,
                 ue->frame_parms.nb_antennas_rx,
-                rx_size_symbol * NR_SYMBOLS_PER_SLOT,
+                rx_size_symbol,
                 false);
 
     uint32_t nvar = 0;
