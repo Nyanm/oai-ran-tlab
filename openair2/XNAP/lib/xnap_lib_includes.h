d3508131389874b29a5624c3d88343e1e9b56ac1
Rakesh BB <rakesh.bb@fsid-iisc.in>
2026-01-30 07:42:12 +0000

    XNAP: add test infra, encode/decode and unit test for Xn Setup Request - Add XNAP library target and CMake integration - New XNAP message type definitions and common utility helpers - Register XNAP as a logging component - Add MACRO_BIT_STRING_T_GNB_ID macro for gNB ID decoding - Implement encoder, decoder + test for Xn Setup Request      Xn Setup Request (3GPP TS 38.423v16.2.0) 	- Global NG-RAN Node ID (M) 	- TAI Support List (M) 	- AMF Region Information (M) - Add equality check and memory management helpers

---
+++ openair2/XNAP/lib/xnap_lib_includes.h
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the OpenAirInterface (OAI) Software Alliance under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The OpenAirInterface Software Alliance licenses this file to You under
+ * the OAI Public License, Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.openairinterface.org/?page_id=698
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *-------------------------------------------------------------------------------
+ * For more information about the OpenAirInterface (OAI) Software Alliance:
+ *      contact@openairinterface.org
+ */
+
+/*! \file xnap_lib_includes.h
+ * \brief XNAP ASN.1 message includes
+ * \author Rakesh BB
+ * \date 2025
+ * \email: rakesh.bb@fsid-iisc.in
+ * \version 1.0
+ * @ingroup _xnap
+ *
+ * This header file contains the includes for the XNAP ASN.1 messages,
+ * generated from the ASN.1 specifications.
+ */
+
+#ifndef XNAP_LIB_INCLUDES_H
+#define XNAP_LIB_INCLUDES_H
+
+#include "XNAP_XnAP-PDU.h"
+#include "XNAP_ProtocolIE-Field.h"
+#include "XNAP_InitiatingMessage.h"
+
+#include "XNAP_GlobalgNB-ID.h"
+#include "XNAP_GlobalNG-RANNode-ID.h"
+#include "XNAP_GlobalAMF-Region-Information.h"
+#include "XNAP_TAISupport-Item.h"
+#include "XNAP_S-NSSAI.h"
+#include "XNAP_BroadcastPLMNinTAISupport-Item.h"
+
+
+#endif // XNAP_LIB_INCLUDES_H
+
