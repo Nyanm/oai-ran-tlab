9581cca17b3e047dde9824767f52cc2b7a2fe820
Raymond Knopp <raymond.knopp@eurecom.fr>
2026-02-25 14:55:10 +0100

    TODO: fix unaligned access. LLR type and optimization

--- openair1/PHY/CODING/nrLDPC_defs.h
+++ openair1/PHY/CODING/nrLDPC_defs.h
@@ -73,7 +73,7 @@ typedef int32_t(LDPC_shutdownfunc_t)(void);
 */
 typedef int32_t(LDPC_decoderfunc_t)(t_nrLDPC_dec_params *p_decParams,
                                     int8_t *p_llr,
-                                    int8_t *p_out,
+                                    uint8_t *p_out,
                                     t_nrLDPC_time_stats *time_stats,
                                     decode_abort_t *ab);
 typedef int32_t(LDPC_encoderfunc_t)(uint8_t **, uint8_t *, encoder_implemparams_t *);
