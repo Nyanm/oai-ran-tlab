e7e4e0562f01987449ccc1649a1f6a7a6bd5b955
Teodora <teodora.vladic@openairinterface.org>
2026-04-03 12:14:24 +0200

    Make debug logging for PRACH nRxPkt = 0
    This commit was tested with MicroAmp FR2 with 2x2 MIMO,
    where PRACH Rx antennas used is 1.
    
    OAI L1 sets the number of PRACH antennas to be the same as for PUSCH.
    Ideally, this should be properly fixed. In the meantime, I modified the
    logging to debug to reduce noise.

--- radio/fhi_72/oaioran.c
+++ radio/fhi_72/oaioran.c
@@ -497,7 +497,7 @@ int xran_fh_rx_prach_read_slot(prach_list_t *prach_list, ru_info_t *ru, int *fra
         int32_t nRxPkt = oran_sync_info.nRxPkt[cc_id][aa][sym_idx];
 #endif
         if (nRxPkt == 0) {
-          LOG_E(HW, "read_prach %d.%d.%d saa = %d: nRxPkt = 0!\n", *frame, *slot, sym_idx, aa);
+          LOG_D(HW, "read_prach %d.%d.%d saa = %d: nRxPkt = 0!\n", *frame, *slot, sym_idx, aa);
           memset(&dst[sym_idx], 0, N_ZC * 2 * sizeof(*dst));
           continue;
         } else if (nRxPkt > 1) { // protection
