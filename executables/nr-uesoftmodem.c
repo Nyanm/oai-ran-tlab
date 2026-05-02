turkeryilmaz/openairinterface5g

Improve UE SIB1 reception
During cell search, the UE could be at any center frequency for a given
bandwidth to perform a SSB scan. If a MIB is found and the PDCCH0 config
indicates that the SIB1 is outside of current bandwidth, a sync_request
indication is sent to PHY with new carrier config so the UE can sync
with SSB and receive SIB1.

To do this, the function get_type0_PDCCH_CSS_config_parameters() is
disaggregated to smaller functions to fill type0 PDCCH config. After MIB
is decoded, we check if type0 PDCCH config in MIB lies within current
bandwidth in function configure_ue_phy_for_sib1_reception().

executables/nr-uesoftmodem.c
@@ -484,6 +484,8 @@ int main(int argc, char **argv)
           uint16_t nr_band = get_softmodem_params()->band;
           mac->nr_band = nr_band;
           mac->ssb_start_subcarrier = UE[CC_id]->frame_parms.ssb_start_subcarrier;
+          mac->N_RB_DL = nrUE_params.N_RB_DL;
+          mac->mu = get_softmodem_params()->numerology;
           nr_init_frame_parms_ue_sa(&UE[CC_id]->frame_parms,
                                     downlink_frequency[CC_id][0],
                                     uplink_frequency_offset[CC_id][0],

Nyanm/oai-ran-tlab

Signed-off-by: Sakthivel Velumani <s.velumani@northeastern.edu>

executables/nr-uesoftmodem.c
@@ -380,6 +380,7 @@ int main(int argc, char **argv)
       mac->nr_band = cell.band;
       mac->ssb_start_subcarrier = cell.ssb_start;
       mac->dl_frequency = cell.rf_frequency;
+      mac->N_RB_DL = cell.N_RB_DL;
 
       UE_CC->sl_mode = get_softmodem_params()->sl_mode;
       init_actor(&UE_CC->sync_actor, "SYNC_", -1);