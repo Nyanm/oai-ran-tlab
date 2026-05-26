acfb208177b8345d88f17172e19f9684a4d1e406
Calvin Peyron <calvin.peyron@open-cells.com>
2026-05-26 16:07:57 +0200

    Fix ue_id retrieval
    Signed-off-by: Calvin Peyron <calvin.peyron@open-cells.com>

--- common/utils/telnetsrv/telnetsrv_ci.c
+++ common/utils/telnetsrv/telnetsrv_ci.c
@@ -140,16 +140,9 @@ int fetch_du_by_ue_id(char *buf, int debug, telnet_printfunc_t prnt)
     MessageDef *msg_p = itti_alloc_new_message (TASK_RRC_GNB, 0, RRC_GET_SINGLE_UE_RNTI);
     itti_send_msg_to_task(TASK_RRC_GNB, 0, msg_p);
     itti_receive_msg(TASK_TELNET, &msg_p);
-    Rrc_get_single_ue_rnti ue;
-    ue.id = msg_p->ittiMsg.rrc_get_single_ue_rnti.id;
-    ue.no_ue = msg_p->ittiMsg.rrc_get_single_ue_rnti.no_ue;
-    ue.rnti = msg_p->ittiMsg.rrc_get_single_ue_rnti.rnti;
-    ue.rrc_ue_id = msg_p->ittiMsg.rrc_get_single_ue_rnti.rrc_ue_id;
-    ue.ue_reconfiguration_counter = msg_p->ittiMsg.rrc_get_single_ue_rnti.ue_reconfiguration_counter;
-    ue.ue_reestablishment_counter = msg_p->ittiMsg.rrc_get_single_ue_rnti.ue_reestablishment_counter;
-    if (ue.no_ue)
+    if (msg_p->ittiMsg.rrc_get_single_ue_rnti.no_ue)
       ERROR_MSG_RET("no single UE in RRC present\n");
-    ue_id = ue.rnti;
+    ue_id = msg_p->ittiMsg.rrc_get_single_ue_rnti.id;
   }
 
   MessageDef *msg_p = itti_alloc_new_message (TASK_RRC_GNB, 0, RRC_GET_DU_ID_BY_RNTI);
