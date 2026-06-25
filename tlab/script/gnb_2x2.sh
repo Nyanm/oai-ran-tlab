b73d55fdf7975e268a59131cf8a4533077a10dc2
nyanm <nyanm2k@gmail.com>
2026-06-25 15:30:31 +0900

    add 2x2 MIMO config files

---
+++ tlab/script/gnb_2x2.sh
@@ -0,0 +1,7 @@
+sudo ~/openairinterface5g/cmake_targets/ran_build/build/nr-softmodem \
+    -O ~/openairinterface5g/targets/PROJECTS/GENERIC-NR-5GC/CONF/gnb.sa.band78.fr1.106PRB.2x2.usrpx310.conf \
+    --gNBs.[0].min_rxtxtime 6 \
+    --usrp-tx-thread-config 1 \
+    -E \
+    --continuous-tx \
+    --T_stdout 2 --T_nowait
